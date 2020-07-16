2366ac04148f7804b87c3c6d5587122edf1434bd
lyx999000 <yexinlu93@gmail.com>
2020-07-16 14:41:25 +1000

    remove https

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -12,10 +12,10 @@ resource "aws_alb" "alb" {
 }
 
 # certificate
-data "aws_acm_certificate" "certificate" {
-  domain   = var.DOMAIN
-  statuses = ["ISSUED", "PENDING_VALIDATION"]
-}
+#data "aws_acm_certificate" "certificate" {
+#  domain   = var.DOMAIN
+#  statuses = ["ISSUED", "PENDING_VALIDATION"]
+#}
 
 # alb listener (https)
 resource "aws_alb_listener" "alb-https" {
@@ -23,7 +23,7 @@ resource "aws_alb_listener" "alb-https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.certificate.arn
+#  certificate_arn   = data.aws_acm_certificate.certificate.arn
 
   default_action {
     target_group_arn = var.DEFAULT_TARGET_ARN
